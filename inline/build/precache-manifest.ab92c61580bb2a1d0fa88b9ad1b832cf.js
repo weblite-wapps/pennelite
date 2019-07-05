59d80fbb6d021c8fde56093caf3f7bc5d720fc0f
javadvhd <vahedi.r46@gmail.com>
2019-07-05 04:51:27 -0700

    imp(up_and_running): react-scripts is added

---
+++ inline/build/precache-manifest.ab92c61580bb2a1d0fa88b9ad1b832cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449acef44f75083e1d1b8ee23dbb2f1f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "78d7d9fc871f67af6b18",
+    "url": "./static/css/main.bce6bc9f.chunk.css"
+  },
+  {
+    "revision": "f25af827f83e7f7515ba",
+    "url": "./static/js/2.a84148d1.chunk.js"
+  },
+  {
+    "revision": "78d7d9fc871f67af6b18",
+    "url": "./static/js/main.54154be5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- inline/src/index.jsx
+++ inline/src/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 // components
-import Root from './root'
+import Root from './setup/root'
 
 const renderPenneliteInline = () =>
   render(
@@ -16,4 +16,4 @@ const renderPenneliteInline = () =>
 renderPenneliteInline()
 
 // Hot Module Replacement API
-if (module.hot) module.hot.accept('./root', renderPenneliteInline)
+if (module.hot) module.hot.accept('./setup/root', renderPenneliteInline)
